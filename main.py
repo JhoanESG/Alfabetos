f024cd325d9cb366247bfb521eb13530330dd2ff
Santiago Ramon <santiramon967@gmail.com>
2024-09-10 15:10:08 -0500

    funcion de generar combinaciones

--- main.py
+++ main.py
@@ -1,4 +1,27 @@
+
+# Función recursiva para generar todas las combinaciones de longitud dada
+def generar_combinaciones(alfabeto, longitud):
+    if longitud == 0:
+        return ['']
+    combinaciones_previas = generar_combinaciones(alfabeto, longitud - 1)
+    nuevas_combinaciones = []
+    
+    for simbolo in alfabeto:
+        for combinacion in combinaciones_previas:
+            nuevas_combinaciones.append(simbolo + combinacion)
+    
+    return nuevas_combinaciones
+
+
+
+
+
 def main():
-    print("Se ejecuta")
+    cantidad_simbolos = int(input("¿Cuántos símbolos tendrá el alfabeto? "))
+
+
+
+
+
 
 main()
