50744c5a922c1b5c49b8d03979fe1b82b7da1296
Santiago Ramon <santiramon967@gmail.com>
2024-09-17 15:33:44 -0500

    se realizo la funcion que realiza las operaciones entre lenguajes

--- parte_b.py
+++ parte_b.py
@@ -6,6 +6,7 @@ def main ():
     lenguaje1 = generar_lenguaje(alfabeto, random.randint(3, 10))
     lenguaje2 = generar_lenguaje(alfabeto, random.randint(3,10))  
     lenguaje3 = generar_lenguaje(alfabeto, random.randint(3,10))  
+
     
 
 
@@ -25,6 +26,38 @@ def definir_alfabeto():
 def generar_lenguaje(alfabeto, longitud):
     return set(generar_combinaciones(alfabeto, longitud))
 
+def unir_lenguajes(lenguaje1, lenguaje2):
+    union= set()
+    for palabra in lenguaje1:
+        union.add(palabra)
+    for palabra in lenguaje2:
+        if palabra not in union:
+            union.add(palabra)
+    
+    return union
 
+def concatenar_lenguajes(lenguaje1,lenguaje2):
+    concatenaciones = set()
+    for palabra1 in lenguaje1:
+        for palabra2 in lenguaje2:
+            concatenaciones.add(palabra1 + palabra2)
+    return concatenaciones
+
+def calcular_cerradura_estrella(lenguaje1):
+    longitud_maxima = 3
+    estrella.add('epsilon')
+    estrella = set()
+    for palabra in lenguaje1:
+        for i in range(1, longitud_maxima + 1):
+            estrella.add(palabra * i)
+    return estrella
+
+def calcular_cerradura_positiva(lenguaje1):
+    longitud_maxima = 3
+    estrella = set()
+    for palabra in lenguaje1:
+        for i in range(1, longitud_maxima + 1):
+            estrella.add(palabra * i)
+    return estrella
     
 main()
