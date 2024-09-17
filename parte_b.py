c13d71d84d1ef2bcc7c75ee25664d64bf080e0f2
Santiago Ramon <santiramon967@gmail.com>
2024-09-17 15:16:28 -0500

    Se realizo la funcion que crea lenguajes a partir de un alfabeto

--- parte_b.py
+++ parte_b.py
@@ -1,4 +1,30 @@
 from main import *
+import random
+
 def main ():
+    alfabeto = definir_alfabeto()
+    lenguaje1 = generar_lenguaje(alfabeto, random.randint(3, 10))
+    lenguaje2 = generar_lenguaje(alfabeto, random.randint(3,10))  
+    lenguaje3 = generar_lenguaje(alfabeto, random.randint(3,10))  
+    
+
+
+# Definir el lenguaje como un conjunto de palabras
+def definir_alfabeto():
+    n = int(input("¿Cuantos simbolos tendrá el alfabeto? "))
+    
+    # Inicializar el conjunto del lenguaje
+    alfabeto = set()
+    
+    for i in range(n):
+        simbolo = input(f"Ingrese el simbolo {i+1}: ")
+        alfabeto.add(simbolo)
+    
+    return alfabeto
+
+def generar_lenguaje(alfabeto, longitud):
+    return set(generar_combinaciones(alfabeto, longitud))
+
+
     
 main()
