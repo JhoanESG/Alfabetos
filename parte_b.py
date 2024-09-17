c885edd598b9327ecc8a21a142aa8c8f5f1bd15c
Jhoan Esteban SG <jhoan011989@gmail.com>
2024-09-17 15:31:41 -0500

    Se crean funciones para transformar cadenas y para mostrar el menu por interfaz grafica

--- parte_b.py
+++ parte_b.py
@@ -1,4 +1,39 @@
-from main import *
+import random
+
 def main ():
+    res = reemplazar_caracteres("Hooooslaed", 'o', 'i')
+    res = invertir_cadena(res)
+    res = reemplazar_por_aleatorio(res)
+    print(res)
+
+def reemplazar_caracteres(cadena, caracter, reemplazo):
+    nueva_cadena = ""
     
+    for c in cadena:
+
+        if c == caracter:
+            nueva_cadena += reemplazo
+        else:
+            nueva_cadena += c 
+    
+    return nueva_cadena
+    
+def invertir_cadena(cadena):
+    nueva_cadena = ""
+    
+    # Recorremos la cadena de manera inversa
+    for i in range(len(cadena)-1, -1, -1):
+        nueva_cadena += cadena[i]
+    
+    return nueva_cadena
+
+def reemplazar_por_aleatorio(cadena):
+    caracter_aleatorio = random.choice(cadena)
+    
+    nueva_cadena = ""
+    for _ in cadena:
+        nueva_cadena += caracter_aleatorio
+    
+    return nueva_cadena
+
 main()
